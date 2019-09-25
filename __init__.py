805513d576739bfcff2d0f1eddbe1d85a202ec7b
Kishan Bangsi <bang.siki2009@gmail.com>
2019-09-25 19:59:47 +0545

    Initalize init file

--- __init__.py
+++ __init__.py
@@ -5,9 +5,9 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config.from_object(Config)
+app.config.from_object(Config) 
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+migrate = Migrate(app, db) 
 login = LoginManager(app)
 login.login_view = 'login'  # Function that forces users to log in before they can view certain page 
 
